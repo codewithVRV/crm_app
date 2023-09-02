be3d0d47cb91b931b4ac1c73169de723a8d04f11
Vishnu Kumar <vermaji1891@gmail.com>
2023-09-02 11:35:19 +0530

    Redux toolkit installation done

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+import store from './Redux/store.js';
+import { Provider } from 'react-redux';
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
