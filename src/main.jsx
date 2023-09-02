982218e0d9e44451dd3a68805ca4e82e0ae9e3c5
Vishnu Kumar <vermaji1891@gmail.com>
2023-09-02 11:52:13 +0530

    added auth Slice

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+
+import {Provider} from 'react-redux'
 import store from './Redux/store.js';
-import { Provider } from 'react-redux';
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+   
     <Provider store={store}>
         <App />
     </Provider>
