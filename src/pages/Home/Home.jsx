092280a63f62bb1dad749776bc6f8d38d8e23936
Vishnu Kumar <vermaji1891@gmail.com>
2023-09-17 11:24:27 +0530

    Added Line Chart Successfully

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import './Home.css'
 
-import { ArcElement, Chart as ChartJS, Legend,Title,Tooltip } from "chart.js";
-import { Pie } from "react-chartjs-2";
+import { ArcElement, BarElement, CategoryScale, Chart as ChartJS,
+ Legend, LinearScale, LineElement, PointElement, Title,Tooltip,} from "chart.js";
+import { useEffect, useState } from 'react';
+import { Line, Pie  } from "react-chartjs-2";
 import { BsFillPencilFill} from 'react-icons/Bs'
 import { TbProgressBolt} from 'react-icons/Tb'
 
@@ -9,12 +11,14 @@ import Card from '../../component/Card/Card';
 import useTickets from '../../hooks/useTickets';
 import HomeLayout from '../../Layouts/HomeLayout';
 
-ChartJS.register(ArcElement, Legend, Title, Tooltip);
+ChartJS.register(ArcElement, Legend, Title, Tooltip,
+     BarElement,CategoryScale, LinearScale, Tooltip, Legend, LineElement,PointElement );
 
 function Home () {
 
     
     const [ticketsState] = useTickets();
+    const [openTickets, setOpenTickets] = useState({});
     // console.log("ticket state is", ticketsState)
 
     const pieChartData = {
@@ -25,12 +29,66 @@ function Home () {
                 lable : "Ticket Distribution",
                 data: Object.values(ticketsState.ticketDistribution),
                 backgroundColor: ["yellow", "red", "green", "blue", "purple", ],
-                borderColor: ["yellow", "red", "green", "blue", "purple",],
-                borderWidth: 1,
+                borderColor: ["black", "white", "black", "white", "black",],
+                borderWidth: 3,
             }
         ]
     }
+    const lineChartData = {
+        labels: Object.keys(openTickets),
+        fontColor: "white",
+        datasets: [
+          {
+            label: 'Open Tickets',
+            data: Object.values(openTickets),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          },
+          
+        ],
+      };
 
+    // const barChartData = {
+    //     labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    //     datasets: [
+    //         {
+    //             lable: 'Open',
+    //             data: [3, 6, 9, 5, 4, 7],
+    //             backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    //             borderColor: 'black',
+    //             borderWidth: 1,
+    //         },
+    //         {
+    //             lable: 'In Progress',
+    //             data: [3, 3, 3, 7, 4, 2],
+    //             backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    //             borderColor: 'black',
+    //             borderWidth: 1,
+    //         },
+    //         {
+    //             lable: 'Resolved',
+    //             data: [3, 3, 3, 7, 4, 2],
+    //             backgroundColor: 'rgba(153, 122, 165, 0.5)',
+    //             borderColor: 'black',
+    //             borderWidth: 1,
+    //         }
+
+    //     ]
+    // }
+
+    useEffect(() => {
+        if(ticketsState.ticketList.length > 0) {
+            let opentTicketsData = {};
+            ticketsState.ticketList.forEach((ticket) => {
+                let date = ticket.createdAt.split("T")[0];
+                if(ticket.status == 'open'){
+                   opentTicketsData[date] = (!opentTicketsData[date]) ? 1 : opentTicketsData[date] + 1;
+                }
+            })
+            setOpenTickets(opentTicketsData)
+        }
+
+    }, [ticketsState.ticketList])
     return (
         <>
         <HomeLayout>
@@ -67,7 +125,18 @@ function Home () {
             <Pie data={pieChartData}/>
         </div>
 
-        
+        <div className="line-chart-data"> 
+            <Line 
+                data={lineChartData}
+            />
+
+        </div>
+
+        {/* <div>
+            <Bar 
+                data={barChartData}
+            />
+        </div> */}
         </>
     )
 }
--- src/routing/MainRoutes.jsx
+++ src/routing/MainRoutes.jsx
@@ -4,6 +4,7 @@ import Login from "../pages/auth/Login";
 import SignUp from "../pages/auth/SignUp";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Home from "../pages/Home/Home";
+// import AuthRoutes from "./AuthRoutes";
 
 
 function MainRoutes () {
@@ -12,6 +13,9 @@ function MainRoutes () {
             <Route path="/" element={ <Home />}/>
             <Route path="/login" element={<Login />}/>
             <Route path="/signup" element={<SignUp />}/>
+            {/* <Route element={<AuthRoutes allowListedRoles={["engineer", "customer"]} />}>
+                <Route path="/resolve" element={<div>Testing</div>} />
+            </Route> */}
             <Route path="/dashboard" element={<Dashboard />}/>
 
         </Routes>
