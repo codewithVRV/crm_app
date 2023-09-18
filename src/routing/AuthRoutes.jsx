b963ec206c80d75c66beba5016e107be88f3b611
Vishnu Kumar <vermaji1891@gmail.com>
2023-09-18 15:52:23 +0530

    React router Outlet based on condition addede

--- src/routing/AuthRoutes.jsx
+++ src/routing/AuthRoutes.jsx
@@ -2,10 +2,11 @@ import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 
 function AuthRoutes ({allowListedRoles}) {
-    const {role} = useSelector((state) => state.auth)
+    
+    const {role} = useSelector((state) => state.auth);
     return (
         <>
-            {allowListedRoles.find((givenRole) => givenRole == role) ? <Outlet/> : <div>denied</div>}
+            {allowListedRoles.find((givenRole) => givenRole == role) ? <Outlet /> : <div>User Not Defined</div>}
         </>
     )
 }
--- src/routing/MainRoutes.jsx
+++ src/routing/MainRoutes.jsx
@@ -4,7 +4,7 @@ import Login from "../pages/auth/Login";
 import SignUp from "../pages/auth/SignUp";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Home from "../pages/Home/Home";
-// import AuthRoutes from "./AuthRoutes";
+import AuthRoutes from "./AuthRoutes";
 
 
 function MainRoutes () {
@@ -13,9 +13,9 @@ function MainRoutes () {
             <Route path="/" element={ <Home />}/>
             <Route path="/login" element={<Login />}/>
             <Route path="/signup" element={<SignUp />}/>
-            {/* <Route element={<AuthRoutes allowListedRoles={["engineer", "customer"]} />}>
-                <Route path="/resolve" element={<div>Testing</div>} />
-            </Route> */}
+            <Route  element={<AuthRoutes allowListedRoles={["engineer", "customer"]}/>}>  
+                <Route path='/resolve' element={<div>Testing Purpose only</div>}/>
+            </Route>
             <Route path="/dashboard" element={<Dashboard />}/>
 
         </Routes>
